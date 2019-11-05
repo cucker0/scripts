85c5cd12b88bc7257d632cd054cc9984f7d3eedd
cucker <hanxiao2100@qq.com>
2019-11-05 19:38:49 +0800

    update generate_markdown_toc.py: 处理标题为链接的特殊情况，修改其他bug

--- other/generate_markdown_toc.py
+++ other/generate_markdown_toc.py
@@ -13,6 +13,7 @@ mail: hanxiao2100@qq.com
 """
 
 import sys
+import re
 
 
 class GenTOC(object):
@@ -22,8 +23,9 @@ class GenTOC(object):
 
     def __init__(self, file_path):
         self.file_path = file_path
-        self.fp = self._open_file() # 打开的文件句柄
-        self.toc = ''  # 目录
+        self.fp = self._open_file()  # 打开的文件句柄
+        self.toc = ''  # 目录内容
+        self.title_info = []  # 用于记录标题信息，结构：[{'title': title, 'level': title_leve}, ]
         self.lines = []  # 接受读取文件所有行
 
     def _open_file(self):
@@ -38,7 +40,7 @@ class GenTOC(object):
             True: 是
             False: 没有生成目录，文件为重写
         """
-        if self.toc:
+        if len(self.title_info) != 0 and self.toc:
             self.fp.seek(0, 0)
             self.fp.writelines(self.lines)
             self.fp.flush()
@@ -52,7 +54,7 @@ class GenTOC(object):
         """处理锚点内容中的特殊字符
 
         如空格改为-
-        , ， 、 ( ) [ ] < > # ! ！ ?特殊字符直接去掉
+        , ， 、 ( ) [ ] < > # ! ！? " ' “ ” ~ + ＋特殊字符直接去掉
 
         :param anchor: str
             锚点内容
@@ -63,6 +65,7 @@ class GenTOC(object):
         ret = anchor.replace(' ', '-')
         ret = ret.replace(',', '')
         ret = ret.replace('，', '')
+        ret = ret.replace('、', '')
         ret = ret.replace('(', '')
         ret = ret.replace(')', '')
         ret = ret.replace('[', '')
@@ -74,10 +77,20 @@ class GenTOC(object):
         ret = ret.replace('？', '')
         ret = ret.replace('!', '')
         ret = ret.replace('！', '')
+        ret = ret.replace('"', '')
+        ret = ret.replace('\'', '')
+        ret = ret.replace('“', '')
+        ret = ret.replace('”', '')
+        ret = ret.replace('~', '')
+        ret = ret.replace('+', '')
+        ret = ret.replace('＋', '')
+
         return ret
 
-    def _toc_concat(self, title, title_leve):
-        """目录拼接
+    def _record_title_info(self, title, title_leve):
+        """记录找到的标题信息
+
+        每调用一次记录一行传递过来的标题信息到self.toc列表中
 
         :param title: str
             标题内容，未过滤处理
@@ -85,14 +98,38 @@ class GenTOC(object):
             标题级别，范围[1, 6]
         :return: None
         """
-        if self.count == 0:
-            self.toc = '# Table Of Contents\n'
-        title = title.split('\n')[0]
-        anchor = self.anchor_filter(title)
+        if len(title) > 0 and (title_leve >= 1 and title_leve <= 6):
+            if len(title.strip()) == 0:
+                return None
+            title_dic = {'title': title, 'level': title_leve}
+            self.title_info.append(title_dic)
+
+    def _toc_concat(self):
+        """目录拼接
+
+        :return: None
+        """
+        if len(self.title_info) == 0:
+            return
+        self.toc = '# Table Of Contents\n'
+
+        highest_title_level = 6
+        # 找出标题级别最高的值
+        for t in self.title_info:
+            if t['level'] < highest_title_level:
+                highest_title_level = t['level']
+
         # 一条目录格式：
         # 空格* [链接文本](#锚点)
-        self.toc = '%s%s* [%s](#%s)\n' %(self.toc, ' ' * 4 * (title_leve - 1), title, anchor)
-        self.count = self.count + 1
+        regex = r"\s*\[(\S+)]((\S+))"  # 标题内容原本就是一个连接，对于这种标题直接保留，如：[...](...)
+        for i in self.title_info:
+            title = i['title'].strip()
+            if re.match(regex, title):
+                self.toc = self.toc + title + '\n'
+            else:
+                anchor = self.anchor_filter(title)
+                level = i['level']
+                self.toc = '%s%s* [%s](#%s)\n' % (self.toc, ' ' * 4 * (level - highest_title_level), title, anchor)
 
     def _find_title(self):
         """查找标题
@@ -131,19 +168,33 @@ class GenTOC(object):
                 title_leve = 3
             elif line.startswith(h2_b2):
                 line_split = [0, self.lines[i - 1]]
+                # 排除```开头的文本块情况
+                if self.lines[i - 1].startswith('```'):
+                    continue
+                # 排除上一行--开头的文本块情况，在sql文本块里为注释
+                if self.lines[i - 1].startswith('--'):
+                    continue
                 title_leve = 2
             elif line.startswith(h2):
                 line_split = line.split(h2)
                 title_leve = 2
             elif line.startswith(h1_b2):
                 line_split = [0, self.lines[i -1]]
+                # 排除上一行```开头的文本块情况
+                if self.lines[i -1].startswith('```'):
+                    continue
+                # 排除上一行--开头的文本块情况，在sql文本块里为注释
+                if self.lines[i -1].startswith('--'):
+                    continue
                 title_leve = 1
             elif line.startswith(h1):
                 line_split = line.split(h1)
                 title_leve = 1
 
             if title_leve != 0:
-                self._toc_concat(line_split[1], title_leve)
+                title = line_split[1].split('\n')[0]  # 把标题的换行符去掉
+                title = title.strip()
+                self._record_title_info(title, title_leve)
 
     def insert_toc(self):
         """向源文件头部插入目录
@@ -152,6 +203,7 @@ class GenTOC(object):
             True: 已插入目录到self.lines
             False: 没有目录要插入
         """
+        self._toc_concat()
         if self.toc:
             self.toc = self.toc + '\n\n'
             self.lines.insert(3, self.toc)
